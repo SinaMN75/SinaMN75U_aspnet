d85677d80115d79e556e88da49cdcdfc90fd5728
Sina MohammadZadeh <sina75mn@outlook.com>
2025-03-16 17:29:12 +0330

    :)

--- InnerServices/LocalizationService.cs
+++ InnerServices/LocalizationService.cs
@@ -6,7 +6,7 @@ public interface ILocalizationService {
 
 public class LocalizationService(IHttpContextAccessor httpContext) : ILocalizationService {
 	public string Get(string key, string? locale) {
-		string filePath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "U_aspnet", "LocalizedMessages.json");
+		string filePath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "SinaMN75U_aspnet", "LocalizedMessages.json");
 		filePath = Path.GetFullPath(filePath);
 		return JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(File.ReadAllText(filePath))!.GetValueOrDefault(locale ?? httpContext.HttpContext?.Request.Headers["Locale"].FirstOrDefault() ?? "en")?.GetValueOrDefault(key, "Error") ?? "Error";
 	}
