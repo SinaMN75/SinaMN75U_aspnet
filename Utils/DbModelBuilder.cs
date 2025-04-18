93c968da92d9ac9b17d0a12a15c23c2fae6efdf3
Sina MohammadZadeh <sina75mn@outlook.com>
2025-04-18 22:20:55 +0330

    :)

--- Utils/DbModelBuilder.cs
+++ Utils/DbModelBuilder.cs
@@ -9,10 +9,10 @@ public static class DbModelBuilder {
 				foreignKey.DeleteBehavior = DeleteBehavior.Cascade;
 			}
 		}
-
+		
 		builder.Entity<UserEntity>().OwnsOne(e => e.JsonData, b => {
 			b.ToJson();
-			b.OwnsMany(i => i.UserAnswerJson).OwnsMany(i => i.Results);
+			b.OwnsMany(i => i.UserAnswerJson).OwnsMany(i => i.Results).OwnsOne(i => i.Answer);
 		});
 		builder.Entity<CategoryEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
 		builder.Entity<MediaEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
