fe39209dc6a72e68e06d10da2532334264eeddf3
Sina MohammadZadeh <sina75mn@outlook.com>
2026-01-30 12:58:32 +0330

    :)

--- Src/Utils/DbModelBuilder.cs
+++ Src/Utils/DbModelBuilder.cs
@@ -6,38 +6,38 @@ public static class DbModelBuilder {
 		foreach (IMutableForeignKey foreignKey in entityType.GetForeignKeys())
 			foreignKey.DeleteBehavior = DeleteBehavior.Cascade;
 
-		builder.Entity<CategoryEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
-		builder.Entity<MediaEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
-		builder.Entity<ContentEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
-		builder.Entity<FollowEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
-		builder.Entity<ContractEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
-		builder.Entity<InvoiceEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
-		builder.Entity<TxnEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
-		builder.Entity<TicketEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
-		builder.Entity<ParkingEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
-		builder.Entity<ParkingReportEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
-		builder.Entity<VehicleEntity>().OwnsOne(e => e.JsonData, b => b.ToJson());
+		builder.Entity<CategoryEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
+		builder.Entity<MediaEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
+		builder.Entity<ContentEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
+		builder.Entity<FollowEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
+		builder.Entity<ContractEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
+		builder.Entity<InvoiceEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
+		builder.Entity<TxnEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
+		builder.Entity<TicketEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
+		builder.Entity<ParkingEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
+		builder.Entity<ParkingReportEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
+		builder.Entity<VehicleEntity>().OwnsOne(e => e.JsonData, b => RelationalOwnedNavigationBuilderExtensions.ToJson(b));
 		builder.Entity<UserEntity>().OwnsOne(e => e.JsonData, b => {
-			b.ToJson();
+			RelationalOwnedNavigationBuilderExtensions.ToJson(b);
 			b.OwnsMany(i => i.UserAnswerJson).OwnsMany(i => i.Results).OwnsOne(i => i.Answer);
 			b.OwnsMany(i => i.VisitCounts);
 		});
 		builder.Entity<ProductEntity>().OwnsOne(e => e.JsonData, b => {
-			b.ToJson();
+			RelationalOwnedNavigationBuilderExtensions.ToJson(b);
 			b.OwnsMany(i => i.VisitCounts);
 			b.OwnsMany(i => i.PointCounts);
 		});
 		builder.Entity<CommentEntity>().OwnsOne(e => e.JsonData, b => {
-			b.ToJson();
+			RelationalOwnedNavigationBuilderExtensions.ToJson(b);
 			b.OwnsMany(i => i.Reacts);
 		});
 		builder.Entity<ExamEntity>().OwnsOne(e => e.JsonData, b => {
-			b.ToJson();
+			RelationalOwnedNavigationBuilderExtensions.ToJson(b);
 			b.OwnsMany(i => i.Questions).OwnsMany(i => i.Options);
 			b.OwnsMany(i => i.ScoreDetails);
 		});
 		builder.Entity<ChatBotEntity>().OwnsOne(e => e.JsonData, b => {
-			b.ToJson();
+			RelationalOwnedNavigationBuilderExtensions.ToJson(b);
 			b.OwnsMany(i => i.History);
 		});
 	}
