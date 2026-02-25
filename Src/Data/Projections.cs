5b1ac20e28cdefae73e5593b3ea51dfef8933cf3
Sina MohammadZadeh <sina75mn@outlook.com>
2026-02-25 17:29:11 +0330

    :)

--- Src/Data/Projections.cs
+++ Src/Data/Projections.cs
@@ -22,7 +22,9 @@ public sealed class ParkingReportSelectorArgs { }
 
 public sealed class VehicleSelectorArgs { }
 
-public sealed class AddressSelectorArgs { }
+public sealed class AddressSelectorArgs {
+	public UserSelectorArgs? Creator { get; set; }
+}
 
 public sealed class TicketSelectorArgs {
 	public MediaSelectorArgs? Media { get; set; }
@@ -82,7 +84,22 @@ public static class Projections {
 			Tags = x.Tags,
 			JsonData = x.JsonData,
 			Title = x.Title,
-			CreatorId = x.CreatorId
+			CreatorId = x.CreatorId,
+			Creator = args.Creator == null
+				? null
+				: new UserResponse {
+					Id = x.Creator.Id,
+					JsonData = x.Creator.JsonData,
+					Tags = x.Creator.Tags,
+					UserName = x.Creator.UserName,
+					PhoneNumber = x.Creator.PhoneNumber,
+					Email = x.Creator.Email,
+					FirstName = x.Creator.FirstName,
+					LastName = x.Creator.LastName,
+					NationalCode = x.Creator.NationalCode,
+					Media = args.Creator.Media == null ? null : x.Creator.Media.AsQueryable().Select(MediaSelector(args.Creator.Media)).ToList(),
+					Categories = args.Creator.Category == null ? null : x.Creator.Categories.AsQueryable().Select(CategorySelector(args.Creator.Category)).ToList()
+				}
 		};
 
 	public static Expression<Func<MediaEntity, MediaResponse>> MediaSelector(MediaSelectorArgs args) =>
--- Src/Data/Responses/AddressResponse.cs
+++ Src/Data/Responses/AddressResponse.cs
@@ -4,6 +4,6 @@ public class AddressResponse : BaseResponse<TagAddress, AddressJson> {
 	public required string Title { get; set; }
 	public string? ZipCode { get; set; }
 	
-	public UserResponse Creator { get; set; } = null!;
+	public UserResponse? Creator { get; set; }
 	public required Guid CreatorId { get; set; }
 }
