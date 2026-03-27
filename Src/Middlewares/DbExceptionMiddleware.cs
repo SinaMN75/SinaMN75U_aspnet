da788a235b4d9753370d30023cce1ddbca08c6d7
Sina MohammadZadeh <sina75mn@outlook.com>
2026-03-27 21:12:25 +0330

    :(

--- Src/Middlewares/DbExceptionMiddleware.cs
+++ Src/Middlewares/DbExceptionMiddleware.cs
@@ -14,27 +14,26 @@ public class DbExceptionMiddleware(RequestDelegate next, ILocalizationService ls
 
 	private async Task HandleDbUpdateExceptionAsync(HttpContext context, DbUpdateException ex) {
 		if (ex.InnerException is PostgresException { SqlState: "23505" } pgEx) {
-			UResponse response = new(
-				Usc.Conflict,
-				pgEx.ConstraintName switch {
-					"IX_Id" => ls.Get("IdIsDuplicated"),
-					"IX_Products_Slug" => ls.Get("SlugIsDuplicated"),
-					"IX_Products_Code" => ls.Get("CodeIsDuplicated"),
-					"IX_Terminal_SimCardNumber" => ls.Get("SimCardNumberIsDuplicated"),
-					"IX_Terminal_SimCardSerial" => ls.Get("SimCardSerialIsDuplicated"),
-					"IX_Terminal_Imei" => ls.Get("ImeiIsDuplicated"),
-					"IX_Txn_TrackingNumber" => ls.Get("TrackingNumberIsDuplicated"),
-					"IX_Users_Email" => ls.Get("EmailIsDuplicated"),
-					"IX_Users_UserName" => ls.Get("UserNameIsDuplicated"),
-					"IX_Users_PhoneNumber" => ls.Get("PhoneNumberIsDuplicated"),
-					"IX_Users_NationalCode" => ls.Get("NationalCodeIsDuplicated"),
-					"IX_Vehicles_NumberPlate" => ls.Get("NumberPlateIsDuplicated"),
-					_ => "A unique constraint violation occurred."
-				}
-			);
-
 			context.Response.StatusCode = StatusCodes.Status400BadRequest;
-			await context.Response.WriteAsJsonAsync(response);
+			await context.Response.WriteAsJsonAsync(new UResponse(
+					status: Usc.Conflict,
+					message: pgEx.ConstraintName switch {
+						"IX_Id" => ls.Get("IdIsDuplicated"),
+						"IX_Products_Slug" => ls.Get("SlugIsDuplicated"),
+						"IX_Products_Code" => ls.Get("CodeIsDuplicated"),
+						"IX_Terminal_SimCardNumber" => ls.Get("SimCardNumberIsDuplicated"),
+						"IX_Terminal_SimCardSerial" => ls.Get("SimCardSerialIsDuplicated"),
+						"IX_Terminal_Imei" => ls.Get("ImeiIsDuplicated"),
+						"IX_Txn_TrackingNumber" => ls.Get("TrackingNumberIsDuplicated"),
+						"IX_Users_Email" => ls.Get("EmailIsDuplicated"),
+						"IX_Users_UserName" => ls.Get("UserNameIsDuplicated"),
+						"IX_Users_PhoneNumber" => ls.Get("PhoneNumberIsDuplicated"),
+						"IX_Users_NationalCode" => ls.Get("NationalCodeIsDuplicated"),
+						"IX_Vehicles_NumberPlate" => ls.Get("NumberPlateIsDuplicated"),
+						_ => "A unique constraint violation occurred."
+					}
+				)
+			);
 			return;
 		}
 
