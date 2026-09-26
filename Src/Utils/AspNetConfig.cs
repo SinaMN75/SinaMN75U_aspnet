c28567bf3efeef94915a8139570ff73e5650cbe4
Sina MohammadZadeh <sina75mn@outlook.com>
2026-09-26 21:19:52 +0330

    :)

--- Src/Utils/AspNetConfig.cs
+++ Src/Utils/AspNetConfig.cs
@@ -5,7 +5,7 @@ public static partial class AspNetConfig {
 		builder.Services.Configure<KestrelServerOptions>(o => o.AllowSynchronousIO = false);
 		builder.Services.Configure<IISServerOptions>(o => o.AllowSynchronousIO = false);
 		builder.Services.AddCors(options => options.AddDefaultPolicy(policy => {
-			if (builder.Environment.IsDevelopment()) policy.AllowAnyOrigin();
+			if (builder.Environment.IsDevelopment() || Core.App.Test) policy.AllowAnyOrigin();
 			else policy.WithOrigins(Core.App.Cors.AllowedOrigins);
 			policy.AllowAnyMethod().AllowAnyHeader();
 		}));
